68f38104b2c4ed4b4f25f1fc9f039fd2ea6b5c8f
whywaita <whywaita@whywrite.it>
2019-01-13 03:28:32 +0900

    using safert sort

--- shokujinjp/sort.go
+++ shokujinjp/sort.go
@@ -2,15 +2,19 @@ package shokujinjp
 
 import (
 	"sort"
+	"strconv"
 )
 
 func SortByCategory(menus []Menu) []Menu {
-	sort.Slice(menus, func(i, j int) bool {
-		return ToCategory(menus[i].Category) < ToCategory(menus[j].Category)
+	sort.SliceStable(menus, func(i, j int) bool {
+		ii, _ := strconv.Atoi(menus[i].Id)
+		jj, _ := strconv.Atoi(menus[j].Id)
+
+		return ii < jj
 	})
 
-	sort.Slice(menus, func(i, j int) bool {
-		return int(menus[i].Id) < int(menus[j].Id)
+	sort.SliceStable(menus, func(i, j int) bool {
+		return ToCategory(menus[i].Category) < ToCategory(menus[j].Category)
 	})
 
 	return menus
